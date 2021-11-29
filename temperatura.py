75782fa6f9a7308edc16d49bf7eae404e0868967
guillesese <guillermosese@hotmail.es>
2021-11-29 20:20:59 +0100

    Add files via upload

--- temperatura.py
+++ temperatura.py
@@ -55,8 +55,8 @@ def calculo_indice_calor(temperatura, humedad):
     return hi
 
 def telegram_bot_sendtext(bot_message):
-    bot_token = ''
-    bot_chatID = ''
+    bot_token = '2122470679:AAGbRfLEm4hjSupP9r6uvsmZFA2q5KziGho'
+    bot_chatID = '3090280'
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
 
     response = requests.get(send_text)
